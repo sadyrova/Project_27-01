c7d71b11607089264c3493af61a410a5e40807df
sadyrova <125734606+sadyrova@users.noreply.github.com>
2023-03-20 12:02:10 +0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 from aiogram import Bot, Dispatcher
+from  aiogram.contrib.fsm_storage.memory import MemoryStorage
 from decouple import config
+
+
+storage = MemoryStorage()
 
 TOKEN = config("TOKEN")
 
 bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=storage)
 ADMINS = (978979476, )
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from aiogram.utils import executor
 from config import dp
-from handlers import client, callback, extra, admin
+from handlers import client, callback, extra, admin, fsm_admin_mentor
 import logging
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
 extra.register_handlers_extra(dp)
+fsm_admin_mentor.register_handlers_fsm_admin_mentor(dp)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)

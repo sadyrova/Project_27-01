b6a44c7b4ee6ef33c9d03f153447d2b26da90cd9
sadyrova <125734606+sadyrova@users.noreply.github.com>
2023-04-01 02:04:14 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from aiogram.utils import executor
-from config import dp
+from config import dp, bot, ADMINS
 from handlers import client, callback, extra, admin, fsm_admin_mentor
 import logging
+from database.bot_db import sql_create
+
+
+async def on_startup(_):
+    await bot.send_message(ADMINS[0], "Я родился!")
+    sql_create()
+
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handlers_admin(dp)
-extra.register_handlers_extra(dp)
 fsm_admin_mentor.register_handlers_fsm_admin_mentor(dp)
+
+extra.register_handlers_extra(dp)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
